43bc3330ded8966aa1ec05efb47ac22f31d95164
Yirga Mulaw <infoyirga12@gmail.com>
2023-10-03 15:15:28 +0300

    Boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Unlock Boxes"""
+
+
+def canUnlockAll(boxes):
+    """Can boxes be unlocked"""
+    if not isinstance(boxes, list) or not all(isinstance(box, list) for box in boxes):
+        return False
+
+    keys = [0]
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys and box < len(boxes):
+                keys.append(box)
+
+    return len(keys) == len(boxes)
