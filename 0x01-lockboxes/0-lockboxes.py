566c2816125051701a510492744dd6fbbd90a023
proyirga <infoyirga12@gmail.com>
2023-12-06 09:25:15 +0300

    Style compliance

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,12 +3,20 @@
 
 
 def canUnlockAll(boxes):
-    """Can boxes be unlocked"""
+    """
+    Can boxes be unlocked
 
+    Args:
+        boxes (list): List of boxes
+
+    Returns:
+        bool: True if boxes can be unlocked, False otherwise
+    """
     if not isinstance(boxes, list) or not all(isinstance(box, list) for box in boxes):
         return False
 
     keys = [0]
+
     for key in keys:
         for box in boxes[key]:
             if box not in keys and box < len(boxes):
