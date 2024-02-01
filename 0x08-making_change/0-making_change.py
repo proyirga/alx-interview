d95b3a0115b33fd643cf2fcb14052b718b9d047a
vboxuser <vboxuser@EDU.myguest.virtualbox.org>
2024-02-01 22:07:01 +0300

    Efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,17 +4,16 @@
 
 
 def makeChange(coins, total):
-    if total <= 0:
-        return 0
+    if total > 0:
+        # An array of the minimum number of coins required to reach each value
+        min_coins = [float('inf')] * (total + 1)
+        min_coins[0] = 0
 
-    # An array of the minimum number of coins required to reach each value
-    min_coins = [float('inf')] * (total + 1)
-    min_coins[0] = 0
+        for coin in coins:
+            for i in range(coin, total + 1):
+                # Update the minimum number of coins required for each value
+                min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
 
-    for coin in coins:
-        for i in range(coin, total + 1):
-            # Update the minimum number of coins required for each value
-            min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
-
-    # Return the minimum number of coins required to reach the total value
-    return min_coins[total] if min_coins[total] != float('inf') else -1
+        # Return the minimum number of coins required to reach the total value
+        return min_coins[total] if min_coins[total] != float('inf') else -1
+    return 0
